bd4defb87f745647a2ddcc8cc05e31be897bc421
ha1rique <vhammes442@gmail.com>
2025-12-18 19:36:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function comprar(produto, preco, idTamanho) {
-  const tamanho = document.getElementById(idTamanho).value;
+const params = new URLSearchParams(window.location.search);
+
+const produto = params.get("produto");
+const preco = params.get("preco");
+const img = params.get("img");
+
+if (produto) {
+  document.getElementById("nomeProduto").innerText = produto;
+  document.getElementById("precoProduto").innerText = `R$ ${preco}`;
+  document.getElementById("imgProduto").src = `imagens/${img}`;
+}
+
+function comprar() {
+  const tamanho = document.getElementById("tamanho").value;
 
   if (tamanho === "Selecione o tamanho") {
     alert("Escolha um tamanho!");
     return;
   }
 
-  const mensagem = `Olá! Quero comprar:\nProduto: ${produto}\nTamanho: ${tamanho}\nPreço: R$ ${preco}`;
+  const mensagem = `Olá! Quero comprar:\n${produto}\nTamanho: ${tamanho}\nPreço: R$ ${preco}`;
 
-  const telefone = "55996982704";
-  const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+  const telefone = "55SEUNUMEROAQUI";
+  window.open(`https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`);
 
-  window.open(url, "_blank");
+  window.location.href = "sucesso.html";
 }
